2a25bec8d1cd46226f7ea019d6a88c49ac2fa98b
IgorBMattiolli <igor.mattiolli@startse.com>
2021-12-16 22:24:17 -0300

    refactor: move components

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Spinner from "@/presentation/components/spinner/spinner";
+import Header from "@/presentation/components/login-header/login-header";
+import Footer from "@/presentation/components/footer/footer";
+import Styles from "./login-styles.scss";
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      <Header />
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Digite seu e-mail" />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <span className={Styles.status}>🔴</span>
+        </div>
+        <button type="submit" className={Styles.submit}>
+          Entrar
+        </button>
+        <span className={Styles.link}>Criar conta</span>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>Erro</span>
+        </div>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default Login;
