11028d838f90f57a4625d8bbffd9f3b62b93505f
IgorBMattiolli <igor.mattiolli@startse.com>
2021-12-18 16:56:32 -0300

    refactor: validation

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { render, RenderResult } from "@testing-library/react";
+import {
+  render,
+  RenderResult,
+  fireEvent,
+  cleanup,
+} from "@testing-library/react";
 import Login from "./login";
+import { Validation } from "@/presentation/protocols/validation";
 
 type SutTypes = {
   sut: RenderResult;
+  validationSpy: ValidationSpy;
 };
 
+class ValidationSpy implements Validation {
+  errorMessage: string;
+  fieldName: string;
+  fieldValue: string;
+
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
+    return this.errorMessage;
+  }
+}
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />);
+  const validationSpy = new ValidationSpy();
+  const sut = render(<Login validation={validationSpy} />);
   return {
     sut,
+    validationSpy,
   };
 };
 
 describe("Login Component", () => {
+  afterEach(cleanup);
   test("Should start with initial state", () => {
     const { sut } = makeSut();
 
@@ -30,5 +51,21 @@ describe("Login Component", () => {
     const passwordStatus = sut.getByTestId("password-status");
     expect(passwordStatus.title).toBe("Campo obrigatório");
     expect(passwordStatus.textContent).toBe("🔴");
+  });
+
+  test("Should call Validation with correct email", () => {
+    const { sut, validationSpy } = makeSut();
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, { target: { value: "any_email" } }); // altera o valor do input
+    expect(validationSpy.fieldName).toBe("email");
+    expect(validationSpy.fieldValue).toBe("any_email");
+  });
+
+  test("Should call Validation with correct password", () => {
+    const { sut, validationSpy } = makeSut();
+    const passwordInput = sut.getByTestId("password");
+    fireEvent.input(passwordInput, { target: { value: "any_password" } });
+    expect(validationSpy.fieldName).toBe("password");
+    expect(validationSpy.fieldValue).toBe("any_password");
   });
 });
