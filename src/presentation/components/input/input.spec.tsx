8f267fe73ba77026d01f418f6f06a7cea550ebd4
IgorBMattiolli <igor.mattiolli@startse.com>
2021-12-22 14:17:00 -0300

    chore: test

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -15,6 +15,6 @@ describe("Input Component", () => {
   test("Should begin with readOnly", () => {
     const sut = makeSut();
     const input = sut.getByTestId("field") as HTMLInputElement;
-    expect(input.readOnly).toBe(false);
+    expect(input.readOnly).toBe(true);
   });
 });
