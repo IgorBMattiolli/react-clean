cfeb0a0f76f3f9ba4418d712655b6e71902af91d
IgorBMattiolli <igor.mattiolli@startse.com>
2021-12-22 14:22:21 -0300

    chore: test

---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Build Status](https://app.travis-ci.com/IgorBMattiolli/react-clean.svg?branch=main)](https://app.travis-ci.com/IgorBMattiolli/react-clean)
--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -15,6 +15,6 @@ describe("Input Component", () => {
   test("Should begin with readOnly", () => {
     const sut = makeSut();
     const input = sut.getByTestId("field") as HTMLInputElement;
-    expect(input.readOnly).toBe(true);
+    expect(input.readOnly).toBe(false);
   });
 });
