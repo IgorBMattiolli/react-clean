1eb450211b1c209348691e3350a18e511ac5a155
IgorBMattiolli <igor.mattiolli@startse.com>
2022-01-13 22:59:00 -0300

    feat: 13/01

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -1,18 +1,22 @@
 import { SurveyModel } from "@/domain/models";
 import faker from "faker";
 
+export const mockSurveyModel = (): SurveyModel => ({
+  id: faker.random.words(),
+  question: faker.random.words(10),
+  answers: [
+    {
+      image: faker.internet.url(),
+      answer: faker.random.words(4),
+    },
+    { answer: faker.random.words(4) },
+  ],
+  date: faker.date.recent(),
+  didAnswer: faker.random.boolean(),
+});
+
 export const mockSurveyListModel = (): SurveyModel[] => [
-  {
-    id: faker.random.words(),
-    question: faker.random.words(10),
-    answers: [
-      {
-        image: faker.internet.url(),
-        answer: faker.random.words(4),
-      },
-      { answer: faker.random.words(4) },
-    ],
-    date: faker.date.recent(),
-    didAnswer: faker.random.boolean(),
-  },
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
 ];
